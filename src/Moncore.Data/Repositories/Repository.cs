f17e2b94c4fda9e311dd24b5bfadf50162922281
Guilherme de Camargo Silva <guisfits@hotmail.com>
2018-01-07 13:29:10 -0200

    repository implementado

--- src/Moncore.Data/Repositories/Repository.cs
+++ src/Moncore.Data/Repositories/Repository.cs
@@ -5,47 +5,68 @@ using System.Threading.Tasks;
 using Moncore.Data.Context;
 using Moncore.Domain.Entities.Base;
 using Moncore.Domain.Interfaces;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Moncore.Data.Repositories
 {
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    internal abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
     {
         private readonly IMongoCollection<TEntity> document;
 
-        public Repository(ApplicationContext context)
+        protected Repository(ApplicationContext context)
         {
             document = context.MongoDb.GetCollection<TEntity>(typeof(TEntity).Name + "s");
         }
 
-        public async Task<ICollection<TEntity>> GetAll()
+        public virtual async Task<ICollection<TEntity>> Get()
         {
             return await document.Find(c => true).ToListAsync();
         }
 
-        public Task<TEntity> Get(int id)
+        public virtual async Task<TEntity> Get(int id)
         {
-            throw new NotImplementedException();
+            return await document.Find(c => c.Id == id).FirstOrDefaultAsync();
         }
 
-        public Task<ICollection<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+        public virtual async Task<ICollection<TEntity>> Get(Expression<Func<TEntity, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return await document.Find(predicate).ToListAsync();
         }
 
-        public Task Add(TEntity obj)
+        public virtual async Task Add(TEntity obj)
         {
-            throw new NotImplementedException();
+            await document.InsertOneAsync(obj);
         }
 
-        public Task<bool> Update(int id, TEntity obj)
+        public virtual async Task AddRange(ICollection<TEntity> objs)
         {
-            throw new NotImplementedException();
+            await document.InsertManyAsync(objs);
         }
 
-        public Task<bool> Delete(int id)
+        public virtual async Task<bool> Update(int id, TEntity obj)
         {
-            throw new NotImplementedException();
+            var actionResult = await document.ReplaceOneAsync(n => n.Id.Equals(id), obj, new UpdateOptions { IsUpsert = true });
+            return actionResult.IsAcknowledged && actionResult.ModifiedCount > 0;
+        }
+
+        public virtual async Task<bool> UpdateRange(Expression<Func<TEntity, bool>> predicate, TEntity obj)
+        {
+            var update = new BsonDocumentUpdateDefinition<TEntity>(obj.ToBsonDocument());
+            var result = await document.UpdateManyAsync(predicate, update);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+
+        public virtual async Task<bool> Delete(int id)
+        {
+            var result = await document.DeleteOneAsync(c => c.Id == id);
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+
+        public virtual async Task<bool> DeleteRange(Expression<Func<TEntity, bool>> predicate)
+        {
+            var result = await document.DeleteManyAsync(predicate);
+            return result.IsAcknowledged && result.DeletedCount > 0;
         }
     }
 }
--- src/Moncore.Domain/Interfaces/IRepository.cs
+++ src/Moncore.Domain/Interfaces/IRepository.cs
@@ -8,12 +8,14 @@ namespace Moncore.Domain.Interfaces
 {
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
-        Task<ICollection<TEntity>> GetAll();
+        Task<ICollection<TEntity>> Get();
         Task<TEntity> Get(int id);
-        Task<ICollection<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
-
+        Task<ICollection<TEntity>> Get(Expression<Func<TEntity, bool>> predicate);
         Task Add(TEntity obj);
+        Task AddRange(ICollection<TEntity> objs);
         Task<bool> Update(int id, TEntity obj);
+        Task<bool> UpdateRange(Expression<Func<TEntity, bool>> predicate, TEntity obj);
         Task<bool> Delete(int id);
+        Task<bool> DeleteRange(Expression<Func<TEntity, bool>> predicate);
     }
 }
